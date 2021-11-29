14d34485ee34a293d1c1da596f9af48918b74bd9
sealuwee <chrisl6uie@gmail.com>
2021-11-29 11:56:39 -0800

    adding changes and notes from class

--- lr.py
+++ lr.py
@@ -1,7 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model as lm
+from sklearn.model_selection import train_test_split
+from statsmodels.regression.linear_model import OLS
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 class LR:
-	def __init__(self, response, eval_metric):
+	global df 
+	# starts as none until df is committed on object instantiation
+	df = None
+
+	def __init__(self, dataset, response, eval_metric):
+		# string that represents the column in the dataset that you would like to specifty as the response.
 		self.response = response
+		# default eval metric = , otherwise unless specified as something else
 		self.eval_metric = eval_metric
+		# string that represents a path to the dataset
+		self.dataset = dataset
+
+
+	def preprocessing(self):
+		PATH = self.dataset
 
 	def show_output():
+		pass
+
+
+
 		
